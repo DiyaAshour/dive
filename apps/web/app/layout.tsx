aee6be7675e08471456609830c71911404aca6cc
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-08-30 00:49:50 +0300

    Load Rewards admin styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -28,6 +28,7 @@ import "./localization.css";
 import "./booking-details.css";
 import "./booking-center.css";
 import "./admin.css";
+import "./admin-rewards.css";
 import "./admin-email.css";
 import "./production-ops.css";
 import "./site-launch.css";
