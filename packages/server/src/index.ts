229d90cbdc6cdd51d42b8f196eb14353f02d0ea7
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-08-29 17:25:04 +0300

    Export public categorized hotel gallery

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -17,6 +17,7 @@ export * from "./discovery/seo";
 export * from "./distribution/google-hotels";
 export * from "./media/service";
 export * from "./media/photo-gallery";
+export * from "./media/public-gallery";
 export * from "./media/cleanup";
 export * from "./storage/provider";
 export * from "./storage/registry";
