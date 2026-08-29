460617ead2b3ce94922c40e50294bf9e0a255c09
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-08-30 02:47:13 +0300

    Export platform access management

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,6 +5,7 @@ export * from "./auth/recovery";
 export * from "./account/service";
 export * from "./admin/authorization";
 export * from "./admin/service";
+export * from "./admin/access";
 export * from "./hotels/authorization";
 export * from "./hotels/service";
 export * from "./hotels/rate-management";
