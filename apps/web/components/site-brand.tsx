0623143bbbee73e6989352d941c65efd898641e7
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-08-30 09:50:04 +0300

    Add dynamic site brand wrapper

---
+++ apps/web/components/site-brand.tsx
@@ -0,0 +1,14 @@
+import {getSiteIdentityConfig} from "@platform/server";
+import {Brand} from "./brand";
+
+type Props = Readonly<{
+  href?: string;
+  inverse?: boolean;
+  compact?: boolean;
+  label?: string;
+}>;
+
+export async function SiteBrand(props: Props) {
+  const identity = await getSiteIdentityConfig();
+  return <Brand {...props} brandName={identity.brandName} logoUrl={identity.logoUrl}/>;
+}
