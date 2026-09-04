01a58b395743baa034bae3bfaa3ae7df146f9dfe
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-09-04 13:23:40 +0300

    Assemble generated Cars hero background

---
+++ apps/web/components/cars-hero-bg/data.ts
@@ -0,0 +1,8 @@
+import c1 from "./chunk-1";
+import c2 from "./chunk-2";
+import c3 from "./chunk-3";
+import c4 from "./chunk-4";
+import c5 from "./chunk-5";
+import c6 from "./chunk-6";
+
+export const CARS_HERO_BACKGROUND_DATA_URI = `data:image/webp;base64,${c1}${c2}${c3}${c4}${c5}${c6}`;
