8cd5765946076621ec6ae7749bd299a1946afa0d
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-09-09 01:17:42 +0300

    feat(nuitee): export webhook processor

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -43,6 +43,7 @@ export * from "./hotelbeds/catalog";
 export * from "./hotelbeds/post-booking";
 export * from "./nuitee/client";
 export * from "./nuitee/checkout-session";
+export * from "./nuitee/webhook";
 export * from "./api-bookings/service";
 export * from "./api-bookings/payment";
 export * from "./api-bookings/voucher";
