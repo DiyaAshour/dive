3af41178aee1a4380fb35f833c6bf38499dc850f
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-09-09 23:20:15 +0300

    Export stored Nuitee preview search

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -44,6 +44,7 @@ export * from "./hotelbeds/post-booking";
 export * from "./nuitee/client";
 export * from "./nuitee/catalog";
 export * from "./nuitee/scheduled-sync";
+export * from "./nuitee/stored-search";
 export {getNuiteeHotelDetailsCatalog as getNuiteeHotelDetails, searchNuiteeCatalog as searchNuitee} from "./nuitee/catalog";
 export * from "./nuitee/checkout-session";
 export * from "./nuitee/webhook";
