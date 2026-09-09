ed739ecceb517e125a4fcb62502c1e8ccc8ad462
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-09-09 21:13:01 +0300

    Load dedicated blog landing styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -17,6 +17,7 @@ import "./wallet.css";
 import "./checkout-wallet-amount.css";
 import "./nuitee-payment.css";
 import "./content.css";
+import "./blog-landing.css";
 import "./blog-cms.css";
 import "./blog-taxonomy.css";
 import "./hotel-vitality.css";
