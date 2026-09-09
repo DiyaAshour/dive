f218df7383122901c83f7aade3219984d03d2800
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-09-09 15:05:46 +0300

    style(nuitee): load payment portal polish

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -15,6 +15,7 @@ import "./trips.css";
 import "./rewards.css";
 import "./wallet.css";
 import "./checkout-wallet-amount.css";
+import "./nuitee-payment.css";
 import "./content.css";
 import "./blog-cms.css";
 import "./blog-taxonomy.css";
